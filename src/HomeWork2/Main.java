abcd7758b71df04496f2a6d00894745f774382dd
SerzhBelik <sergeybelik86@gmail.com>
2018-08-24 19:20:43 +0400

    написан класс обработки ошибки размера массива

--- src/HomeWork2/Main.java
+++ src/HomeWork2/Main.java
@@ -5,14 +5,20 @@ public class Main {
     public static void main(String[] args) {
 
         String[][] arr = new String[4][4];
+        try{
+            converStringArrayToInt(arr);
+        } catch (MyArraySizeException e){
+            System.out.println("Размер массива задан не верно!");
+        }
 
-        converStringArrayToInt(arr);
 
 
 
     }
 
-    public static int converStringArrayToInt(String[][] arr){
+    public static int converStringArrayToInt (String[][] arr) throws MyArraySizeException {
+
+        if(arr.length!=4||arr[0].length!=4) throw new MyArraySizeException();
         return 1;//FIXME
     }
 }
