168a9f23c2a0e649161291270e94a802829727f3
SerzhBelik <sergeybelik86@gmail.com>
2018-08-24 18:13:51 +0400

    каракас метода мэйн создан

---
+++ src/HomeWork2/Main.java
@@ -0,0 +1,18 @@
+package HomeWork2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[][] arr = new String[4][4];
+
+        converStringArrayToInt(arr);
+
+
+
+    }
+
+    public static int converStringArrayToInt(String[][] arr){
+        return 1;//FIXME
+    }
+}
