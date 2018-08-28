9e570bedcfb2a01836661916574645599e049d85
SerzhBelik <sergeybelik86@gmail.com>
2018-08-28 21:40:27 +0400

    класс телефонная книга написан и проверен

--- src/HomeWork3/Main.java
+++ src/HomeWork3/Main.java
@@ -2,7 +2,7 @@ package HomeWork3;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
+
 
 public class Main {
 
@@ -10,13 +10,29 @@ public class Main {
 	String[] names = {"Andreev", "Ivanov", "Anikeev", "Volodin", "Maximov", "Smirnov", "Antonov", "Andreev", "Anikeev", "Petrov",
                       "Sergeev", "Alexandrov", "Volodin", "Maximov", "Alexandrov", "Ivanov", "Petrov", "Andreev", "Ivanov", "Smirnov"};
 
-	List<String> lastnames = Arrays.asList(names);
-	HashSet<String> ln = new HashSet<>(lastnames);
+	String[] phoneNumbers = {"8-901-111-11-11", "8-901-111-11-22", "8-901-111-11-33", "8-901-111-11-44", "8-901-111-11-55",
+                             "8-901-111-11-66", "8-901-111-11-77", "8-901-111-11-88", "8-901-111-11-99", "8-901-111-11-00",
+                             "8-901-111-22-11", "8-901-111-33-11", "8-901-111-44-11", "8-901-111-55-11", "8-901-111-66-11",
+                             "8-901-111-77-11", "8-901-111-88-11", "8-901-111-99-11", "8-901-111-00-11", "8-901-222-11-11"};
+
+
+	HashSet<String> ln = new HashSet<>(Arrays.asList(names));
 
 	getUniqueLastname(ln);
 
 	getReiteration(ln, names);
+
+	Phonebook phonebook = new Phonebook();
+
+	for (int i = 0; i<names.length; i++){
+	    phonebook.add(names[i], phoneNumbers[i]);
     }
+
+    phonebook.get("Ivanov");
+
+    }
+
+
 
 
 
@@ -29,13 +45,14 @@ public class Main {
             }
             System.out.println(lastname+" встречается " + reiterations + "раз(а)");
         }
+        System.out.println();
     }
 
 
-    
+
     private static void getUniqueLastname(HashSet<String> ln) {
 
-        System.out.println(ln);
+        System.out.println(ln + "\n");
     }
 
 
---
+++ src/HomeWork3/Phonebook.java
@@ -0,0 +1,28 @@
+package HomeWork3;
+
+import java.util.*;
+
+public class Phonebook {
+    Map<String, List<String>> phoneMap = new HashMap<>();
+
+
+     void add(String k, String v){
+        if(this.phoneMap.containsKey(k)) {
+            phoneMap.get(k).add(v);
+            return;
+        }
+        List<String> tempList = new ArrayList<>();
+        tempList.add(v);
+        phoneMap.put(k, tempList);
+    }
+
+   void get(String k){
+       System.out.println(k);
+       for (String s : phoneMap.get(k)){
+           System.out.println(s);
+       }
+    }
+
+
+
+}
