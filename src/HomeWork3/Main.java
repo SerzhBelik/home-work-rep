1c864302b14904297a81ea67175650adc9fe3c68
SerzhBelik <sergeybelik86@gmail.com>
2018-08-28 19:54:14 +0400

    вывод уникальных значенй массива

--- src/HomeWork3/Main.java
+++ src/HomeWork3/Main.java
@@ -1,8 +1,26 @@
 package HomeWork3;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	String[] names = {"Andreev", "Ivanov", "Anikeev", "Volodin", "Maximov", "Smirnov", "Antonov", "Andreev", "Anikeev", "Petrov",
+                      "Sergeev", "Alexandrov", "Volodin", "Maximov", "Alexandrov", "Ivanov", "Petrov", "Andreev", "Ivanov", "Smirnov"};
+
+	List<String> lastnames = Arrays.asList(names);
+
+	getUniqueLastname(lastnames);
+
+	
     }
+
+    private static void getUniqueLastname(List<String> lastames) {
+        HashSet<String> ln = new HashSet<>(lastames);
+        System.out.println(ln);
+    }
+
+
 }
