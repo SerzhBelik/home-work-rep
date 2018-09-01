68d6cb8c930f465b19bca933ecafd9cb71a84378
SerzhBelik <sergeybelik86@gmail.com>
2018-09-01 12:54:26 +0400

    дз окончено

--- HW4/src/HomeWork3/СhatWindow.java
+++ HW4/src/HomeWork3/СhatWindow.java
@@ -11,6 +11,13 @@ import java.awt.event.KeyEvent;
 public class СhatWindow extends JFrame {
     final static int WIDTH = 400;
     final static int HEIGHT = 600;
+
+    JTextArea textArea = new JTextArea();
+    JPanel panel = new JPanel();
+    JTextArea inputText = new JTextArea(1, 30);
+    JButton enter = new JButton("Enter");
+    JScrollPane jsp = new JScrollPane(inputText);
+
     public СhatWindow(){
         setTitle("Home work chat");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -18,34 +25,31 @@ public class СhatWindow extends JFrame {
      Toolkit.getDefaultToolkit ().getScreenSize ().height/2-HEIGHT/2, WIDTH, HEIGHT);
         setResizable(false);
 
-
-        setLayout(new BorderLayout());
-        JTextArea textArea = new JTextArea();
-        JPanel panel = new JPanel();
-        JTextArea inputText = new JTextArea(2, 30);
-        JButton enter = new JButton("Enter");
-        JScrollPane jsp = new JScrollPane(inputText);
-        ActionListener listner = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                setText(inputText.getText());
-            }
-        };
-
         textArea.setEditable(false);
-        textArea.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        textArea.setLineWrap(true);
+        textArea.setBorder(BorderFactory.createEtchedBorder(Color.WHITE, new Color(150, 200, 150)));
+        textArea.setBackground(new Color(250, 255, 250));
         inputText.setLineWrap(true);
         inputText.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
-                super.keyPressed(e);
+
+                if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    e.consume();
+                    setText(inputText.getText());
+                }
             }
         });
-        jsp.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
         jsp.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        enter.addActionListener(listner);
+        enter.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setText(inputText.getText());
+            }
+        });
 
-
+        setLayout(new BorderLayout());
         add(textArea, BorderLayout.CENTER);
         add(panel, BorderLayout.SOUTH);
 
@@ -58,8 +62,10 @@ public class СhatWindow extends JFrame {
     }
 
     private void setText(String text) {
+        textArea.append("Name: " + text+ "\n"+ "\n");
+        inputText.setText(null);
+        inputText.setCaretPosition(0);
 
-       System.out.println(text);
     }
 
 
