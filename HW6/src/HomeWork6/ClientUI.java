e913980b1b2a1b96bf49c16ce66f7ab95b20322d
SerzhBelik <sergeybelik86@gmail.com>
2018-09-10 13:05:21 +0400

    пусто

--- HW6/src/HomeWork6/ClientUI.java
+++
@@ -1,7 +0,0 @@
-package HomeWork6;
-
-public interface ClientUI {
-    void showUI();
-
-    void addMessage(String w);
-}
--- HW6/src/HomeWork6/Controller.java
+++
@@ -1,9 +0,0 @@
-package HomeWork6;
-
-public interface Controller {
-    void sendMessage(String msg);
-
-    void closeConnection();
-
-    void showUI(ClientUI clientUI);
-}
