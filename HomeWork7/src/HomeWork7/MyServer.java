7972dc64e031506a65b14734c6626fddaffff136
SerzhBelik <sergeybelik86@gmail.com>
2018-09-12 18:54:59 +0400

    выносим поиск адресата в отдельный метод

--- HomeWork7/src/HomeWork7/MyServer.java
+++ HomeWork7/src/HomeWork7/MyServer.java
@@ -1,18 +1,16 @@
 package HomeWork7;
 
-import javax.swing.*;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Scanner;
+
 
 
 public class MyServer {
-//    private Scanner cs = new Scanner(System.in);
+
     private ServerSocket serverSocket;
     private List<ClientHandler> clients = new ArrayList<>();
     private  AuthService authService;
@@ -21,36 +19,20 @@ public class MyServer {
         this.authService = authService;
         try {
             serverSocket = new ServerSocket(8189);
-
             System.out.println("Server is started");
-//            socket = serverSocket.accept();
-//            System.out.println("client connect");
-
-//            new Thread(()-> scanConsole()).start();
-
-//            ConsoleThread ct = new ConsoleThread(socket); // поток для консоли
-//            ct.start();
-
-//            while (true) {
-//                if (scanner.hasNextLine()) {
-//                    String text = scanner.nextLine();
-//                    if (text.equals("end")) break;
-
-//                    if (text.length() > 6 && text.substring(0, 7).equals("@Client")) {
-////
-//                        System.out.println(text);
-//                        continue;
-//                    }
-//                    printWriter.println((new Date().toString()) + "\n" + "Name: " + text + "\n");
-//
-//                }
-//            }
-
-
-
         }catch (IOException e){
             System.out.println("Server isn't started");
+            this.close();
         }
+    }
+
+    private void close() {
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.exit(0);
     }
 
 
@@ -103,28 +85,41 @@ public class MyServer {
         String[] commands = str.split(" ", 3);
         String nick = commands[1];
         System.out.println(commands[2]);
+        if(isUserFound(nick)){
         for (ClientHandler client : clients) {
+            if (nick.equals(senderName)) {
+                client.sendMessage("Why do you write to yourself?");
+                return;
+            }
             if (nick.equals(client.getNick())) {
                 client.sendMessage((new Date().toString()) + "\n"
                         + "PM from " + senderName + ": " + commands[2] + "\n");
             }
+
             if (senderName.equals(client.getNick())) {
-                client.sendMessage((new Date().toString())+ "\n"
+                client.sendMessage((new Date().toString()) + "\n"
                         + "PM to " + nick + ": " + commands[2] + "\n");
             }
 
         }
+    } else {
+            for (ClientHandler client : clients) {
+                if (senderName.equals(client.getNick())){
+                   client.sendMessage("User not found!");
+                }
+            }
+        }
     }
 
-//        private void scanConsole() {
-//            while (true) {
-//                String msg = cs.nextLine();
-//                if (msg != null && !msg.isEmpty()) {
-//                    printWriter.println("User: " + msg);
-//                    if (msg.equals("end")) break;
-//                }
-//            }
-//        }
+    private boolean isUserFound(String nick) {
+        for (ClientHandler client : clients) {
+            if (nick.equals(client.getNick())) return true;
+        }
+
+        return false;
     }
+
+
+}
 
 
