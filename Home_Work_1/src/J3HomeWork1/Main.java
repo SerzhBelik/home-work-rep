d55f7241323aa71bbf1f44202e5f6238f0fe3739
SerzhBelik <sergeybelik86@gmail.com>
2018-09-23 18:21:17 +0400

    п.2 готов

--- Home_Work_1/src/J3HomeWork1/Main.java
+++ Home_Work_1/src/J3HomeWork1/Main.java
@@ -1,6 +1,8 @@
 package J3HomeWork1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
@@ -8,13 +10,28 @@ public class Main {
     String[] arr1 = {"Ivan" , "Petr", "Anton"};
     Integer[] arr2 = {1, 2 , 3};
 
+
 	changeIndexes(arr1, 0, 2);
 	changeIndexes(arr2, 0, 2);
+
+	ArrayList list1 = convertArrayToList(arr1);
+	ArrayList list2 = convertArrayToList(arr2);
+
     }
 
     public  static <Arr> void changeIndexes( Arr[] arr, int index1, int index2){
         Arr tmp = arr[index1];
         arr[index1] = arr[index2];
         arr[index2] = tmp;
+    }
+
+    public static <Arr> ArrayList convertArrayToList(Arr[] arr){
+        ArrayList<Arr> list = new ArrayList<>();
+        for (Arr a : arr) {
+            list.add(a);
+        }
+        return list;
+
+
     }
 }
